94c51fd5b05636336d476bb0d5cef07ad3317b5e
acikek <spiralixp@gmail.com>
2023-05-12 09:38:10 -0700

    yes

--- src/main/java/com/acikek/pt/block/ModBlocks.java
+++ src/main/java/com/acikek/pt/block/ModBlocks.java
@@ -1,13 +1,13 @@
 package com.acikek.pt.block;
 
 import com.acikek.pt.PT;
-import com.acikek.pt.core.api.refined.RefinedStateTypes;
+import com.acikek.pt.core.api.refined.RefinedStateType;
 import net.minecraft.block.Block;
 import net.minecraft.sound.BlockSoundGroup;
 
 public class ModBlocks {
 
-    public static final Block EMPTY_SACK = new Block(RefinedStateTypes.POWDER.getBaseSettings().sounds(BlockSoundGroup.WOOL));
+    public static final Block EMPTY_SACK = new Block(RefinedStateType.POWDER.getBaseSettings().sounds(BlockSoundGroup.WOOL));
 
     public static void register() {
         PT.REGISTRY.registerBlock("empty_sack", EMPTY_SACK);
